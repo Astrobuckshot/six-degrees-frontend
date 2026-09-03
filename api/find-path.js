0bea4fcdb2785df0da92916b59c5a3b9c44cc888
Your Name <you@example.com>
2026-09-02 22:32:10 -0700

    Revert "Add year labels to connection display, dedupe against legacy context years"
    This reverts commit 1f4be2af7f4ab06784c627bc323c4a1206a69be8.

--- api/find-path.js
+++ api/find-path.js
@@ -48,7 +48,6 @@ export default async function handler(req, res) {
       photoUrl: row.photo_url,
       connectionType: row.connection_type,
       context: row.context,
-      date: row.connection_date,
       sourceLink: row.source_link,
       showSourceLink: row.show_source_link,
     }));
